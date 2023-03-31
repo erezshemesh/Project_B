10aad58f1c199bfe0298b33c09266fa5764ddbc7
dans8 <danielst@campus.technion.ac.il>
2023-03-31 14:40:55 +0300

    Added marks

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from openpyxl import load_workbook
 import os
 from torch.utils.tensorboard import SummaryWriter
 
-#Erez Shemesh
+#Erez Shemesh 2
 # consts:
 DAY_BEGIN_SEC = 21600
 DAY_END_SEC = 86400
